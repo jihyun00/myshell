be2fc25cac80aaddf406e76ae198ba3fa1e47d7a
jihyun <noblea1117@gmail.com>
2015-05-24 22:03:31 +0900

    write moved path

--- cd.c
+++ cd.c
@@ -1,12 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 void cd(char **argv) {
+	//TODO: move before path
 	if(sizeof(argv) == 1) {
+		int fd, n;
+		char *buf, *token, *content;
+
+		if(access(dir[index], F_OK) == -1) {
+			fd = open("./history.txt", O_CREAT | O_RWDR);
+		} else {
+			fd = open("./history.txt", O_RWDR);
+		}
+
+		if(strcmp(argv[0], "~") == 0) {
+			char *homePath = getenv("HOME"); //get home path	
+			chdir(homePath);
+		}
+
 		chdir(argv[0]);
 
-	} else if(sizeof(argv) == 2) {
+		n = read(fd, content, sizeof(content));
+		if(n == -1) {
+			perror("read");
+			exit(1);
+		}
+		content[n] = '\0';
 
+		token = strtok(content, "/");
+		while(token != NULL) { //check saved path count
+			index++;
+			token = strtok(NULL, "/");
+		}
+
+		if(index > 50) { // if count is over 50, exit
+			return;
+		}
+
+		sprintf(buf, "/%s", argv[0]);
+
+		if(write(fd, buf, sizeof(buf)) != sizeof(buf)) {
+			perror("write");
+			return;
+		}
+
+	} else if(sizeof(argv) == 2) {
+		if(strcmp(argv[0], "-b")== 0) {
+			int i, num;
+			num = atoi(argv[1]);
+
+			if(num < 0) {
+				printf("Invalid Value\n");
+				return;
+			}
+
+		} else {
+			printf("Invalid Option\n");
+			return;
+		}
 	}
 }
