9384934c679cd2dd877468e892fe1dd2c284377a
jihyun <noblea1117@gmail.com>
2015-05-24 21:15:00 +0900

    add mkdir command p option

--- myls.c
+++ myls.c
@@ -5,20 +5,13 @@
 void myls(char **argv) {
 	struct dirent *dent;
 	DIR *dp;
-	// 아무 인자도 받지 않았을 경우
-	if(argv == NULL) {
-		if((dp = opendir("./")) == NULL) {
-			exit(1);
-		}
 
-		while((dent = readdir(dp)) != NULL) {
-			printf("%s\t", dent->d_name);
-		}
-		printf("\n");
-
-	} else {	
-		
-
+	if((dp = opendir("./")) == NULL) {
+		exit(1);
 	}
-	
+
+	while((dent = readdir(dp)) != NULL) {
+		printf("%s\t", dent->d_name);
+	}
+	printf("\n");
 }
--- mymkdir.c
+++ mymkdir.c
@@ -1,28 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 void mymkdir(char **argv) {
 	int err;
+	char *token;
+	char *dirname;
+	char **dir;
+	int index = 0;
 
 	if(sizeof(argv) == 1) {
-		char *dirname = argv[0];
+		dirname = argv[0];
 
+		err = mkdir(dirname, 0777);
+		if(err == -1) {
+			perror("mkdir");
+			exit(1);
+		}
 	} else if(sizeof(argv) == 2) {
 		if(strcmp(argv[0], "-p") == 0) {
-			dirname = argv[1];
-			// 고민해보기
-		} else {
-			// ERROR
+			token = strtok(argv[1], "/");
+			while(token != NULL) {
+				dir[index++] = token;
+				token = strtok(NULL, "/");
+			}
+			int i;
+			for(i = 0; i < index-1; i++) {
+				if(access(dir[0], F_OK) == -1) {
+					perror("access");
+					exit(1);
+				} else {
+					mkdir(dir[index]);
+					chdir(dir[index]);
+				}
+			}
+			int j;
+			for(j = 0; j < index-1; j++) {
+				chdir("../");
+			}
 		}
-
-	} else {
-		//ERROR
-	}
-
-	err = mkdir(dirname, 0777);
-	if(err == -1) {
-		perror("mkdir");
-		exit(1);
 	}
 }
